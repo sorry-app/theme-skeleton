04c83757344bc2cb8a331176704edf631cdf9c13
Robert Rawlins <robert@weboffins.com>
2014-03-31 14:53:38 +0100

    Added the bare bones src directory structure for the theme.

---
+++ src/assets/status-page.js
@@ -0,0 +1,7 @@
+/*
+	This is a skeleton Javascript file for the theme. Put your
+	own JS in here or replace the file with your own scripts.
+*/
+window.onload = function() {
+	// Handler for when the window loads.
+};
